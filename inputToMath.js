eb3e82355e6391e264f5f1d1aedfa8a6ff850020
Pedro Henrique <pedro.26.3.h@gmail.com>
2024-09-02 17:39:12 -0300

    Add cancel package, add meta tags

--- inputToMath.js
+++ inputToMath.js
@@ -2,6 +2,11 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputArea = document.querySelector('.input textarea');
     const outputBox = document.querySelector('.output .box');
 
+    window.mathJax = {
+        loader: {load: ['[tex]/cancel']},
+        tex: {packages: {'[+]': ['cancel']}}
+    };
+
     inputArea.addEventListener('input', function(){
         const inputText = inputArea.value;
         outputBox.innerHTML = inputText; // a query escolhida é const, mas os atributos da classe são mutáveis.
